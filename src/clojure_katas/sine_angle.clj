45e443d13614fdbda45ed1d527f8e6c9fe61b23b
Shkodran Gerguri <shkodran.gerguri@gooddata.com>
2015-01-21 09:45:27 +0100

    sine_angle solution

--- src/clojure_katas/sine_angle.clj
+++ src/clojure_katas/sine_angle.clj
@@ -6,4 +6,11 @@
   by making use of the approximation sin x  x if x is sufficiently small,
   and the trigonometric identity:
     sin(r) = 3sin(r/3) -. 4sin^3(r/3)"
-  [angle])
+  [angle]
+  (let [cube   #(* % % %)
+        sinefn #(- (* 3 %1) (* 4 (cube %1)))]
+    (loop [r angle
+           level 1]
+      (if (<= (Math/abs r) 0.1)
+        (last (take level (iterate sinefn r)))
+        (recur (/ r 3.0) (inc level))))))
